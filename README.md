19577d8b949a4f19b7b0580331d46ba58dc57cb8
shinwachi <shinwachi@gmail.com>
2019-02-16 15:25:25 -0500

    added knime webservice example

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # kdl
+
+
+
+## Knime Webservice Example
+
+In /docker folder: an example of dockerized web service which runs knime workflow.
+
+To test the dockerized web service, use curl or postman:
+
+```bash
+curl -X POST \
+  http://localhost:8080/api/v0/workflow \
+  -H 'Postman-Token: 9d1a019f-fbed-4ed3-be9c-5495611bc432' \
+  -H 'cache-control: no-cache' \
+  -d '[ {  "Name" : "Aa",  "Type" : "a",  "Score" : 6}, {  "Name" : "Bb",  "Type" : "a",  "Score" : 3}, {  "Name" : "Cc",  "Type" : "a",  "Score" : 4}, {  "Name" : "Dd",  "Type" : "a",  "Score" : 2}, {  "Name" : "Aa",  "Type" : "b",  "Score" : 1}, {  "Name" : "Cc",  "Type" : "b",  "Score" : 9}, {  "Name" : "Dd",  "Type" : "b",  "Score" : 3} ]'
+```
+
+Output:
+
+```json
+[ {
+  "Name" : "Aa",
+  "a" : 6.0,
+  "b" : 1.0
+}, {
+  "Name" : "Bb",
+  "a" : 3.0
+}, {
+  "Name" : "Cc",
+  "a" : 4.0,
+  "b" : 9.0
+}, {
+  "Name" : "Dd",
+  "a" : 2.0,
+  "b" : 3.0
+} ]
+```
--- docker/server/server.py
+++ docker/server/server.py
@@ -1,9 +1,54 @@
 from flask import Flask
+from flask import request
+import uuid
+import subprocess
+from subprocess import Popen
+import simplejson as json
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return "hello world"
+
+@app.route('/api/v0/workflow', methods=['POST'])
+def api_v0_workflow():
+	uid = str(uuid.uuid4())
+	INFILE_PATH  = "/dev/shm/input.json"
+	OUTFILE_PATH = "/dev/shm/output.json"
+
+	payload = request.get_data()
+
+	
+	with open(INFILE_PATH, "wb") as infile:
+		infile.write(payload)
+	cmd = '/knime_3.7.1/knime -nosplash -reset -nosave -application org.knime.product.KNIME_BATCH_APPLICATION -workflowFile=/worker/workflow.knwf'.split()
+	process = Popen(cmd)
+
+	process.wait(timeout=100)
+	with open(OUTFILE_PATH) as outfile:
+		output=outfile.read()
+
+	# try:
+	# 	process.wait(timeout=100)
+	# 	with open(OUTFILE_PATH):
+	# 		output=myfile.read()
+
+	# except subprocess.TimeoutExpired:
+	# 	process.terminate()
+	# 	output = json.dumps({"error": "timeout"})
+	# 	response = app.response_class(
+	# 		response = output,
+	# 		status=500,
+	# 		mimetype="application/json")
+	# 	return response
+
+	response = app.response_class(
+		response = output,
+		status=200,
+		mimetype="application/json")
+
+	return response
 
 
 if __name__ == "__main__":
